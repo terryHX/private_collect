ae7b0b1ecdb51b5e98182d193b728f82549af33d
智天 <terry.hx@alibaba-inc.com>
2016-08-09 18:48:01 +0800

    sf

---
+++ launchApplicationTest/mytest2.h
@@ -0,0 +1,13 @@
+//
+//  mytest2.h
+//  launchApplicationTest
+//
+//  Created by TERRY on 16/8/9.
+//  Copyright © 2016年 huangxun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mytest2 : NSObject
+
+@end
---
+++ launchApplicationTest/mytest2.m
@@ -0,0 +1,13 @@
+//
+//  mytest2.m
+//  launchApplicationTest
+//
+//  Created by TERRY on 16/8/9.
+//  Copyright © 2016年 huangxun. All rights reserved.
+//
+
+#import "mytest2.h"
+
+@implementation mytest2
+
+@end
