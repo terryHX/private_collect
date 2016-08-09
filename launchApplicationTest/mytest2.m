719d2bfa9001272f2e51e7e54ee0ef806b3a4d33
智天 <terry.hx@alibaba-inc.com>
2016-08-09 19:38:34 +0800

    sfa

--- launchApplicationTest/mytest2.m
+++ launchApplicationTest/mytest2.m
@@ -14,6 +14,7 @@
 {
      NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");
      NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");
+      NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");  NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");  NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");  NSLog(@"asfasdfsdfadsf"); NSLog(@"asfasdfsdfadsf");
     return;
 }
 
