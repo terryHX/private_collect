d0660009502ad6d33c3ada65ff8e3790962b78a4
智天 <terry.hx@alibaba-inc.com>
2016-08-11 15:50:40 +0800

    xc

--- launchApplicationTest/helloWorld.m
+++ launchApplicationTest/helloWorld.m
@@ -37,5 +37,12 @@
     NSLog(@"asfasdfsdfadsf");
     NSLog(@"asfasdfsdfadsf");
     NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
+    NSLog(@"asfasdfsdfadsf");
 }
 @end
