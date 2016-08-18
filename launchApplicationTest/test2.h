ca184e033c0704f6cb6e9c9b75bbfc77320a7213
huangxun <hx@huangxundemacbook-air.local>
2016-08-18 13:49:06 +0800

    pdf

---
+++ launchApplicationTest/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  launchApplicationTest
+//
+//  Created by huangxun on 16/8/18.
+//  Copyright © 2016年 huangxun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test2 : NSObject
+
+@end
---
+++ launchApplicationTest/test2.m
@@ -0,0 +1,23 @@
+//
+//  test2.m
+//  launchApplicationTest
+//
+//  Created by huangxun on 16/8/18.
+//  Copyright © 2016年 huangxun. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+- (void)commonInit
+{
+    NSLog(@"");
+    NSLog(@"");
+    NSLog(@"");
+    NSLog(@"");
+    NSLog(@"");
+    
+}
+
+@end
