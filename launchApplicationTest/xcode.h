62072ebd23a21dbb849583b0d51930c7ad7e22bd
智天 <terry.hx@alibaba-inc.com>
2016-11-21 10:40:55 +0800

    a

--- launchApplicationTest/xcode.h
+++ launchApplicationTest/xcode.h
@@ -14,6 +14,8 @@
 @property (nonatomic,assign) NSString *y2;
 
 @property (nonatomic,assign) NSString *y3;
+@property (nonatomic,assign) NSString *y4;
+@property (nonatomic,assign) NSString *y5;
 
 
 @end
