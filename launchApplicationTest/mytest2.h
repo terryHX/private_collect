160716b588feb888eef41197c4baf7b80b67fccd
智天 <terry.hx@alibaba-inc.com>
2016-11-21 10:44:25 +0800

    s

--- launchApplicationTest/mytest2.h
+++ launchApplicationTest/mytest2.h
@@ -9,5 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface mytest2 : NSObject
+@property (nonatomic,assign) NSString *y1;
+@property (nonatomic,assign) NSString *y2;
+
+
 
 @end
--- launchApplicationTest/sdfsd.h
+++ launchApplicationTest/sdfsd.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface sdfsd : NSObject
-
+@property (nonatomic,assign) NSString *y1;
+@property (nonatomic,assign) NSString *y2;
 @end
